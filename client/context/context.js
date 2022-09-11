371d2199a006d932cc15d4431a155daec5d8585b
Rishabh Pant <81542717+RishabhXD@users.noreply.github.com>
2022-09-11 23:26:19 +0530

    mid way

--- client/context/context.js
+++ client/context/context.js
@@ -1,67 +1,67 @@
-import { createContext, useState, useEffect, useReducer } from 'react'
-import { useRouter } from 'next/router'
-import Gun from 'gun'
+import { createContext, useState, useEffect, useReducer } from "react";
+import { useRouter } from "next/router";
+import Gun from "gun";
 
-export const DiscordContext = createContext()
+export const DiscordContext = createContext();
 
-const gun = Gun(['https://discord-gun-node.herokuapp.com/gun'])
+const gun = Gun(["https://discord-gun-node.herokuapp.com/"]);
 
-const initialState = { messages: [] }
+const initialState = { messages: [] };
 
 const reducer = (state, action) => {
   try {
-    if (action.type == 'clear') return { messages: [] }
-    if (action.type == 'add')
-      return { messages: [...state.messages, action.data] }
+    if (action.type == "clear") return { messages: [] };
+    if (action.type == "add")
+      return { messages: [...state.messages, action.data] };
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
 export const DiscordProvider = ({ children }) => {
-  const router = useRouter()
-  const [state, dispatch] = useReducer(reducer, initialState)
-  const [currentAccount, setCurrentAccount] = useState('')
-  const [roomName, setRoomName] = useState('')
-  const [placeholder, setPlaceholder] = useState('Message...')
-  const [messageText, setMessageText] = useState('')
-  const [currentUser, setCurrentUser] = useState()
+  const router = useRouter();
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [roomName, setRoomName] = useState("");
+  const [placeholder, setPlaceholder] = useState("Message...");
+  const [messageText, setMessageText] = useState("");
+  const [currentUser, setCurrentUser] = useState();
 
   useEffect(() => {
-    checkIfWalletIsConnected()
-  }, [])
+    checkIfWalletIsConnected();
+  }, []);
 
   useEffect(async () => {
-    if (!currentAccount) return
+    if (!currentAccount) return;
 
     try {
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/getCurrentUserData?account=${currentAccount}`,
-      )
+        `${process.env.NEXT_PUBLIC_API_URL}/getCurrentUserData?account=${currentAccount}`
+      );
 
-      const data = await response.json()
-      setCurrentUser(data)
+      const data = await response.json();
+      setCurrentUser(data);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }, [currentAccount])
+  }, [currentAccount]);
 
   useEffect(() => {
-    setRoomName(router.query.name)
-    dispatch({ type: 'clear', data: {} })
-    setPlaceholder(`Message ${router.query.name}`)
-    setMessageText('')
-    getMessages()
-  }, [router.query])
+    setRoomName(router.query.name);
+    dispatch({ type: "clear", data: {} });
+    setPlaceholder(`Message ${router.query.name}`);
+    setMessageText("");
+    getMessages();
+  }, [router.query]);
 
   const getMessages = () => {
-    const _name = router.query.name
-    const _roomId = router.query.id
-    const messagesRef = gun.get(_name)
+    const _name = router.query.name;
+    const _roomId = router.query.id;
+    const messagesRef = gun.get(_name);
 
-    messagesRef.map().once(message => {
+    messagesRef.map().once((message) => {
       dispatch({
-        type: 'add',
+        type: "add",
         data: {
           sender: message.sender,
           content: message.content,
@@ -69,77 +69,77 @@ export const DiscordProvider = ({ children }) => {
           createdAt: message.createdAt,
           messageId: message.messageId,
         },
-      })
-    })
-  }
+      });
+    });
+  };
 
   const createUserAccount = async (userAddress = currentAccount) => {
-    if (!window.ethereum) return
+    if (!window.ethereum) return;
 
     try {
       const data = {
         userAddress: userAddress,
-      }
+      };
 
       try {
         await fetch(`${process.env.NEXT_PUBLIC_API_URL}/createuser`, {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(data),
-        })
+        });
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
 
       try {
         await fetch(`${process.env.NEXT_PUBLIC_API_URL}/createdm`, {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(data),
-        })
+        });
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   const checkIfWalletIsConnected = async () => {
-    if (!window.ethereum) return
+    if (!window.ethereum) return;
     try {
       const addressArray = await window.ethereum.request({
-        method: 'eth_accounts',
-      })
+        method: "eth_accounts",
+      });
       if (addressArray.length > 0) {
-        setCurrentAccount(addressArray[0])
-        createUserAccount(addressArray[0])
+        setCurrentAccount(addressArray[0]);
+        createUserAccount(addressArray[0]);
       } else {
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   const connectWallet = async () => {
-    if (!window.ethereum) return
+    if (!window.ethereum) return;
     try {
       const addressArray = await window.ethereum.request({
-        method: 'eth_requestAccounts',
-      })
+        method: "eth_requestAccounts",
+      });
 
       if (addressArray.length > 0) {
-        setCurrentAccount(addressArray[0])
-        createUserAccount(addressArray[0])
+        setCurrentAccount(addressArray[0]);
+        createUserAccount(addressArray[0]);
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   return (
     <DiscordContext.Provider
@@ -158,5 +158,5 @@ export const DiscordProvider = ({ children }) => {
     >
       {children}
     </DiscordContext.Provider>
-  )
-}
+  );
+};
--- server/server.js
+++ server/server.js
@@ -10,11 +10,12 @@ app.use(cors())
 app.use(Gun.serve)
 
 app.get('/', (_, res) => {
-  res.status(200).send('> DEBUG: Discord Node is Live')
+  res.status(200).send('> DEBUG: E-Classes Node is Live')
 })
 
 const server = app.listen(port, () => {
-  console.log(`> DEBUG: Discord Node is listening at http://localhost:${port}`)
+  console.log(`Listening at http://localhost:${port}`)
 })
 
 Gun({ web: server })
+   
